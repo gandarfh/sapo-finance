88bae860b038a9937ebad9c993643d5baf1d6fec
Joao Ferreira <joao.ferreira@housi.com.br>
2020-11-12 01:57:36 -0300

    refact: change knobs for the native controllers in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,18 +2,5 @@
 
 module.exports = {
   stories: ['../**/*.stories.tsx'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-knobs',
-  ],
-  typescript: {
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      compilerOptions: {
-        allowSyntheticDefaultImports: false,
-        esModuleInterop: false,
-      },
-    },
-  },
+  addons: ['@storybook/addon-essentials'],
 }
