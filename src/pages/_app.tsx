d732677fb6ed0f00bf66373358591d36cc255dc7
Joao Ferreira <joao.ferreira@housi.com.br>
2020-11-12 02:25:21 -0300

    feat(pwd): add pwd for the project and finish boilerplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,8 @@ function App({ Component, pageProps }: AppProps) {
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500&display=swap"
           rel="stylesheet"
-        ></link>
+        />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A simple project to work with typescript, React, NextJs and Styled Component"
